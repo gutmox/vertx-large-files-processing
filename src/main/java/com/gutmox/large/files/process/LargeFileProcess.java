1689f8cab9778310254fc7dab3a606e52fc59c99
Pablo Gutierrez <gutmox@gmail.com>
2020-09-01 18:28:06 +0200

    A better improved code

--- src/main/java/com/gutmox/large/files/process/LargeFileProcess.java
+++ src/main/java/com/gutmox/large/files/process/LargeFileProcess.java
@@ -1,17 +1,32 @@
 package com.gutmox.large.files.process;
 
 import io.reactivex.Completable;
+import io.reactivex.Flowable;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.file.AsyncFile;
 
 public class LargeFileProcess {
 
-	Completable doProcess(Vertx vertx){
-		OpenOptions oo = new OpenOptions();
-		return vertx.fileSystem().rxOpen("small_file.txt", oo).flatMapCompletable(file ->{
-			file.toFlowable().buffer(300).forEach(data -> System.out.println("Read data: " + data));
+	Completable doProcess(Vertx vertx) {
 
-			return Completable.complete();
-		}).doOnError(Throwable::printStackTrace);
+		OpenOptions oo = new OpenOptions();
+		return vertx.fileSystem().rxOpen("semi-large.txt", oo)
+			.doOnError(Throwable::printStackTrace)
+			.flatMapPublisher(AsyncFile::toFlowable).doOnError(Throwable::printStackTrace)
+			.flatMap(buffer -> Flowable.fromArray(buffer.toString().split("\n")))
+			.flatMapCompletable(line -> {
+				System.out.println(line);
+				return Completable.complete();
+			});
+	}
+
+	Completable doProcessSmall(Vertx vertx) {
+		OpenOptions oo = new OpenOptions();
+		return vertx.fileSystem().rxOpen("small_file.txt", oo).doOnError(Throwable::printStackTrace)
+			.flatMapCompletable(file -> {
+				file.toFlowable().buffer(300).forEach(data -> System.out.println("Read data: " + data));
+				return Completable.complete();
+			}).doOnError(Throwable::printStackTrace);
 	}
 }
